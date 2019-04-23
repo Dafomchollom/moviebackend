e550afd1247f5061137c253f65125194bb4f1331
Dafomchollom <ayomidechollom@gmail.com>
2019-04-23 20:27:54 +0100

    initial3

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category
 # Register your models here.
 class Articles_admin(admin.ModelAdmin):
     list_display = ('id','title','postdate','Post_status')
@@ -7,3 +7,8 @@ class Articles_admin(admin.ModelAdmin):
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
 admin.site.register(Article, Articles_admin)
+
+class category_admin(admin.ModelAdmin):
+    list_display = ('id','name','slug',)
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Category, category_admin)
--- articles/models.py
+++ articles/models.py
@@ -1,7 +1,20 @@
 from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
+from django.utils import timezone
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
     STATUS_CHOICES = (
@@ -9,12 +22,12 @@ class Article(models.Model):
         ('published', 'Published'),
     )
     title = models.CharField(max_length=1000)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     thumbnail = models.ImageField(default='default.png',blank=True)
     body = RichTextUploadingField()
     releasedate = models.DateField()
     dateseen = models.DateField()
-    postdate = models.DateField()
+    postdate = models.DateTimeField(default=timezone.now)
     Post_status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     
     def __str__(self):
